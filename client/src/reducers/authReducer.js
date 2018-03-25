f57ed85aa2a89c593e02ea15101a57d39449b277
Marian Serna <marianhalliday@gmail.com>
2018-03-25 19:07:39 -0400

    Modifying action to return only the data in the response + Setting up auth reducer to return only null, the action, or false based on whether or not the user is logged in

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,11 @@
-export default function(state = {}, action) {
-  console.log(action);
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      // return user model or false if user is not logged in
+      return action.payload || false;
+
     default:
       return state;
   }
