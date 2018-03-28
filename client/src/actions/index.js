5aef621ccaa18b313ad3aa0d3300c2034ec77762
Marian Serna <marianhalliday@gmail.com>
2018-03-28 17:08:24 -0400

    Deleting comment

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,11 +2,6 @@ import axios from 'axios';
 
 import { FETCH_USER } from './types';
 
-// actionCreator purpose: returns an action (js object with type property and optionally a payload as well)
-// redux thunk allows to break the rule of returning an action right away. In this case, the action creator doesn't return an action but it passes it on to the dispatch function (function that belongs to the redux store)
-// dispatch function called with an action: action is forwarded to all reducers in the app
-// only thing redux-thunk does is giving us access to the dispatch function
-
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
 
