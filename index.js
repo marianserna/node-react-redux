293068131db454073a2f2a7a8d70d82a6eaf64fb
Marian Serna <marianhalliday@gmail.com>
2018-03-28 16:51:47 -0400

    Adding configuration to commit client to Heroku

--- index.js
+++ index.js
@@ -35,6 +35,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+// for express to behave corrently in prod
+if (process.env.NODE_ENV === 'production') {
+  // make sure express will serve up prod assets(eg: main.js or main.css)
+  app.use(express.static('client/build'));
+  // express will serve up index.html if it doesnt recognize the route (comes from react)
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 // if (process.env.NODE_ENV === 'production') {
