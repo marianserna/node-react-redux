9afbd4ef52b68012dc756aab55b6fd2d1711b89e
Marian Serna <marianhalliday@gmail.com>
2018-03-29 15:33:07 -0400

    Configuring SendGrid + Creating Mailer

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+
 // if required directly there'll be an issue with testing mongoose
 const Survey = mongoose.model('surveys');
 
 // Create survey and send a big email (many recipients)
 module.exports = app => {
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('Thanks for voting!');
+  });
   // 1. user must be logged in
   // 2. user has enough credits to send a survey
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-    const { title, subject, body, recipient } = req.body;
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+    const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
       title,
@@ -20,5 +26,20 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+    // send the email
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+
+    try {
+      await mailer.send();
+      // persist survey to db
+      await survey.save();
+      // subtract credits when survey has been sent
+      req.user.credits -= 1;
+      const user = await req.user.save;
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
